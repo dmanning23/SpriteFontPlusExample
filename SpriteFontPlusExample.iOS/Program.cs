e9ee17fd07654a96fbbbc27a00ff90af289b6cfc
Dan Manning <dmanning23@hotmail.com>
2019-03-13 13:57:37 -0500

    added iOS example

---
+++ SpriteFontPlusExample.iOS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DynamicSpriteFontExample;
+using Foundation;
+using UIKit;
+
+namespace SpriteFontPlusExample.iOS
+{
+	[Register("AppDelegate")]
+	class Program : UIApplicationDelegate
+	{
+		private static Game1 game;
+
+		internal static void RunGame()
+		{
+			game = new Game1();
+			game.Run();
+		}
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			UIApplication.Main(args, null, "AppDelegate");
+		}
+
+		public override void FinishedLaunching(UIApplication app)
+		{
+			RunGame();
+		}
+	}
+}
